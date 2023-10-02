1bfc08a1cea0e26a245b075b3571ab71a6890c4e
Sourabh Maurya <shmaurya2014.com>
2023-10-02 23:25:10 +0530

    out of bound exception

--- src/main/java/com/driver/Gmail.java
+++ src/main/java/com/driver/Gmail.java
@@ -60,6 +60,7 @@ public class Gmail extends Email {
     public String findLatestMessage(){
         // If the inbox is empty, return null
         // Else, return the message of the latest mail present in the inbox
+        if(inbox.size()==0) return null;
  return inbox.get(inbox.size()-1).message;
     }
 
